5e83ee2df86f1b0d03fff70dc9df5203493afb34
jalvarezotero <you@example.com>
2018-11-19 11:18:27 +0100

    end

---
+++ Bolwtin10_2/src/bolwtin10_2/xogo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bolwtin10_2;
+
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jalvarezotero
+ */
+public class xogo {
+
+    public void inicio() {
+        Random aleatorio = new Random();
+        int num = 1 + aleatorio.nextInt(50);
+        boolean correcto = false;
+        do {
+            int respuesta = Integer.parseInt(JOptionPane.showInputDialog("Adivine un número entre 1 y 50:"));
+            if (respuesta == num) {
+                JOptionPane.showMessageDialog(null, "Respuesta correcta");
+                correcto = true;
+                break;
+            } else if (Math.abs(num - respuesta) <= 5) {
+                JOptionPane.showMessageDialog(null, "Muy cerca");
+            } else if (Math.abs(num - respuesta) <= 10) {
+                JOptionPane.showMessageDialog(null, "Cerca");
+            } else if (Math.abs(num - respuesta) <= 20) {
+                JOptionPane.showMessageDialog(null, "Lejos");
+            } else if (Math.abs(num - respuesta) > 20) {
+                JOptionPane.showMessageDialog(null, "Muy lejos");
+            }
+
+        } while (correcto == false);
+    }
+}
